9461a2b59df14624776d627a8d5537ec7047e890
josevasconcelos2002 <zevasconcelos6@gmail.com>
2022-04-27 17:22:39 +0100

    Atualização

---
+++ Módulo D/Aula 1/aula1_ex1.js
@@ -0,0 +1,30 @@
+/* // console.log("O comando está a funcionar.");
+var vel = 49.5;  //é assim ( console.log(string)  ) que se escreve na tela do node.js//
+
+if(vel > 50) {   //condição composta
+
+    console.log(`A velocidade a que o seu carro vai é de ${vel} Km/h ... Tenha cuidado, já ultrapassou o limite de velocidade!`);
+
+}
+
+else {
+
+    console.log("Está dentro do limite de velocidade, porém, dirija sempre com o cinto de segurança colocado.");
+
+}
+
+*/
+
+var país = "Portugal";
+
+if(país == "Portugal") {
+
+    console.log("Português");
+
+}
+else {
+
+    console.log("Estrangeiro");
+
+}
+
